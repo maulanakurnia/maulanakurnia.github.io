12b473931de29d7c9584a4958d05db8148cbcff9
Maulana Kurnia <50382874+maulanakurnia@users.noreply.github.com>
2019-07-22 05:27:06 +0700

    Update custom.js

--- js/custom.js
+++ js/custom.js
@@ -1,7 +1,14 @@
-jQuery(document).ready(function(){
-	jQuery('.progress-horizontal').each(function(){
-		jQuery(this).find('.progress-horizontal__progress ').animate({
-			width:jQuery(this).attr('data-percent')
-		},6000);
-	});
+var skillsDiv = jQuery('#skills');
+
+jQuery(window).on('scroll', function() {
+  var winT = jQuery(window).scrollTop(),
+    winH = jQuery(window).height(),
+    skillsT = skillsDiv.offset().top;
+  if (winT + winH > skillsT) {
+    jQuery('.progress-horizontal').each(function() {
+      jQuery(this).find('.progress-horizontal__progress').animate({
+        width: jQuery(this).attr('data-percent')
+      }, 6000);
+    });
+  }
 });
