ed0d6e237dfd9dc4f8bce8e9c778e456cd818956
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-07-07 23:40:49 +0800

    add link

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Box, Text, Button, Flex, Heading} from '@primer/components';
+import {Box, Text, Button, Flex, Heading, Link} from '@primer/components';
 import {ThemeProvider} from 'styled-components'
 import {MarkGithubIcon} from   '@primer/octicons-react'
 import { Ripple } from 'react-spinners-css';
@@ -41,33 +41,44 @@ class App extends Component {
           <Flex justifyContent="center">
           </Flex>
           <Flex flexWrap="nowrap" justifyContent="center">
-          <Button sx={{
+          <Button as={Link} href="https://www.twitter.com/maulanaakurniaa" target="_blank" sx={{
             color: '#ffffff',
             backgroundColor: '#158feb',
             border: '1px solid #158feb',
             borderRadius: '7px',
             boxShadow: '0px 1px 0px rgba(20,70,32,0.1), inset 0px 2px 0px rgba(255,255,255,0.03)',
             ':hover' : {
+              textDecoration: 'none',
               backgroundColor: '#42a8f5',
               boxShadow: '0px 1px 0px rgba(20,70,32,0.1), inset 0px 2px 0px rgba(255,255,255,0.03)',
+              transform: 'translate3d(0, -2px, 0)',
+              webkitTransform: 'translate3d(0, -2px, 0)'
             }
           }}>
             <FontAwesomeIcon icon={faTwitter} /> Twitter
           </Button>
 
-          <Button mx={2} sx={{
+          <Button mx={2} as={Link} href="https://github.com/maulanakurnia" target="_blank" sx={{
             color: '#ffffff',
             backgroundColor: '#24292E',
             border: '1px solid #24292E',
             borderRadius: '7px',
             boxShadow: '0px 1px 0px rgba(20,70,32,0.1), inset 0px 2px 0px rgba(255,255,255,0.03)',
             ':hover' : {
+              textDecoration: 'none',
               backgroundColor: '#383d42',
               boxShadow: '0px 1px 0px rgba(20,70,32,0.1), inset 0px 2px 0px rgba(255,255,255,0.03)',
+              transform: 'translate3d(0, -2px, 0)',
+              webkitTransform: 'translate3d(0, -2px, 0)'
             }
           }}><MarkGithubIcon size={16} py={-12}/> Github</Button>
 
-          <Button sx={{
+          <Button as={Link} href="https://instagram.com/maulanaakurniaa" target="_blank"sx={{
+            ':hover' : {
+            textDecoration: 'none',
+            transform: 'translate3d(0, -2px, 0)',
+            webkitTransform: 'translate3d(0, -2px, 0)'
+            },
             borderRadius: '7px'
           }}>
             <FontAwesomeIcon icon={faInstagram} /> Instagram
