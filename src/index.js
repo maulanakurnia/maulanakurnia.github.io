583d400ab2b497748a789620242eb71ede4e778c
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-07-07 23:23:01 +0800

    Uwu Uwu

--- src/index.js
+++ src/index.js
@@ -3,10 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+const warningTitleCSS = 'color:red; font-size:60px; font-weight: bold; -webkit-text-stroke: 1px black;';
+const warningDescCSS = 'font-size: 18px;';
+console.log('%cStop!', warningTitleCSS);
+console.log("%cThis is a browser feature intended for developers!!!", warningDescCSS);
+const publicIp = require('public-ip');
+ 
+(async () => {
+    console.log("Opps, Kami berhasil mengkap Public IP Adress anda {{ ", await publicIp.v4(), " }}");
+    //=> '46.5.21.123'
+ 
+    // console.log(await publicIp.v6());
+    //=> 'fe80::200:f8ff:fe21:67cf'
+})();
 
 ReactDOM.render(
   <App />,
-  document.getElementById('root')
+  document.getElementById('root'),
+
 );
 
 // If you want your app to work offline and load faster, you can change
