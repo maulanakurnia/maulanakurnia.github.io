32f33c96a2a8e9edaf05a7810920250bd1a931e4
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-07-09 06:42:12 +0800

    add Cover Component

---
+++ src/components/HomeComp/CoverComp.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const CoverComp = styled.div`
+    background-attachment: fixed;
+    background-position: center center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    display: block;
+    width: 100%;
+    object-fit: cover;
+    min-height: 300px;
+    height: calc(30vh);
+    border-bottom: 1px solid #839296;
+    background-image: url(${props => props.imageUrl});
+`;
+
+export default CoverComp;
