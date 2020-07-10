bfb7b2685bd6687242d5a5f8e3241b5e4f4158c7
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-07-10 20:33:19 +0800

    remove Devicon and DiMysql

--- src/components/HomeComp/CardComp.jsx
+++ src/components/HomeComp/CardComp.jsx
@@ -1,8 +1,7 @@
 import React, {Component} from 'react';
 import styled from 'styled-components';
-import DevIcon from "devicon-react-svg";
 import { Box } from "@primer/components";
-import { DiJavascript1, DiPhp, DiJava, DiMysql,DiSass, DiLinux, DiGit} from "react-icons/di";
+import { DiJavascript1, DiPhp, DiJava, DiSass, DiLinux, DiGit} from "react-icons/di";
 const Circle = styled.div`
   /* width: 50px;
   height: 50px;
@@ -54,17 +53,18 @@ const Card = styled.div`
   justify-content: center;
   align-items: center;
   position: relative;
-  margin-bottom: 15px;
-  /* box-shadow: 0 14px 26px rgba(0,0,0,0.04); */
-  transition: all 0.3s ease-out;
+  margin: 5px;
+  /* padding: 5px; */
+  transition-duration: 5s;
+  -webkit-transition: all 1s ease;
+  -moz-transition: all 1s ease;
+  -o-transition: all 1s ease;
+  -ms-transition: all 1s ease;
+  transition: all 1s ease;
+  vertical-align: top;
   text-decoration: none;
-  margin-left: 20px;
-  &:hover {
-    text-decoration: none!important;
-    border: 1px solid  ${props => props.bgColorLight};
-    transform: translateY(-5px) scale(1.005) translateZ(0);
-    box-shadow: 0 24px 36px rgba(0,0,0,0.11),
-    0 24px 46px ${props => props.boxShadowColor};
+  &:hover{
+    cursor: pointer;
   }
   &:hover ${Overlay}{
     transform: scale(4) translateZ(0);
@@ -89,14 +89,22 @@ class CardComp extends Component {
     render() { 
         return (  
         <Box sx={{
+          display: 'inline-block',
+          justifyContent: 'center!important',
+          
+          '@media screen and (min-width: 320px) and (max-width: 568px)': {
+            position:"absolute",
+            left: '15%',
+            transform: 'translate(-5%, -0%)',
+          },
           '@media screen and (min-width: 360px) and (max-width: 640px)': {
             position:"absolute",
-            left: '20%',
+            left: '25%',
             transform: 'translate(-15%, -0%)',
           },
           '@media screen and (min-width: 411px) and (max-width: 731px)': {
             position:"absolute",
-            left: '20%',
+            left: '25%',
             transform: 'translate(-10%, -0%)',
           },
           '@media screen and (min-width: 732px)': {
@@ -106,26 +114,22 @@ class CardComp extends Component {
           },
           '@media screen and (min-width: 768px) and (max-width: 1024px)': {
             position:"absolute",
-            left: '10%',
-            transform: 'translate(5%, -0%)',
+            left: '5%',
+            transform: 'translate(2%, -0%)',
           },
           '@media screen and (min-width: 1024px) and (max-width: 1366px)': {
             position:"absolute",
-            left: '5%',
-            transform: 'translate(-0%, -30%)',
-          },
-          // '@media screen and (max-width: 823px)': {
-          //   marginLeft: '25px'
-          // }
+            left: '12%',
+            transform: 'translate(-0%, -50%)',
+          }
         }}>
-        {/* <div>  */}
-            <Card bgColorLight={"#020202"} bgColor={"#020202"} boxShadowColor={""}>
+            <Card bgColorLight={"#020202"} bgColor={"#020202"} boxShadowColor={"#020202"}>
                 <Overlay bgColor={"#020202"}/>
                 <Circle bgColor={"#020202"}>
                     <DiLinux as={"svg"} size={80} color={"#020202"}/>
                 </Circle>
             </Card> 
-            <Card bgColorLight={"#e94e32"} bgColor={"#e94e32"} boxShadowColor={""}>
+            <Card bgColorLight={"#e94e32"} bgColor={"#e94e32"} boxShadowColor={"#e94e32"}>
                 <Overlay bgColor={"#e94e32"}/>
                 <Circle bgColor={"#e94e32"}>
                     <DiGit as={"svg"} size={80} color="e94e32"/>
@@ -149,19 +153,12 @@ class CardComp extends Component {
                     <DiJava as={"svg"} size={80} color={"#e61f24"}/>
                 </Circle>
             </Card> 
-            {/* <Card bgColorLight={"#005e87"} bgColor={"#005e87"} boxShadowColor={"#4d598f8f"}>
-                <Overlay bgColor={"#005e87"}/>
-                <Circle bgColor={"#005e87"}>
-                    <DiMysql as={"svg"} size={80} color={"#005e87"}/>
-                </Circle>
-            </Card>  */}
             <Card bgColorLight={"#c76495"} bgColor={"#c76495"} boxShadowColor={""}>
                 <Overlay bgColor={"#c76495"}/>
                 <Circle bgColor={"#c76495"}>
                     <DiSass as={"svg"} size={80} color={"#c76495"}/>
                 </Circle>
             </Card> 
-        {/* </div>  */}
        </Box>
         );
     }
