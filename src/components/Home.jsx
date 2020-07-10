9879b7d16cd33e0dd6b013e2e4b04d37b53db923
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-07-10 20:32:59 +0800

    add aos

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,8 +12,16 @@ import uAvatar from "../assets/img/profile.jpeg"
 import ButtonComp from "./HomeComp/ButtonComp"
 import Cover from './HomeComp/CoverComp';
 import CardComp from "./HomeComp/CardComp";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 class Home extends Component {
+  componentDidMount(){
+    AOS.init({
+      duration : 2000
+    })
+  }
+
   render() {
     return (
       <div>
@@ -26,19 +34,25 @@ class Home extends Component {
                                                         borderRight: "1px solid #839296"}}>
                 </Avatar>
             </Flex>
-            <Heading as={Flex} color="white" justifyContent="center" mt={1} fontSize={30} fontWeight="700" >Maulana Kurnia</Heading>
-            <Text as={Flex} justifyContent="center" fontSize={15} color="grey" fontStyle="italic">IT Student at UPN "Veteran" Yogyakarta</Text>
-            <ButtonComp/>
-            
-            <Heading as={Flex} color="white" justifyContent="center" mt={50} fontSize={30} fontWeight="700" sx={{
-            '@media screen and (min-width: 823px)': {
-              marginBottom: "100px"
-            },
-            '@media screen and (max-width: 823px)': {
-              marginBottom: '20px'
-            }
-            }}>My Game</Heading>
-            <CardComp/>
+            <div data-aos='fade-up' data-smooth-scroll-offset="180" data-aos-easing="ease" data-aos-duration="4000" data-aos-delay="0">
+              <Heading as={Flex} color="white" justifyContent="center" mt={1} fontSize={30} fontWeight="700" sx={{
+
+              }}>Maulana Kurnia</Heading>
+              <Text as={Flex} justifyContent="center" fontSize={15} color="grey" fontStyle="italic">IT Student at UPN "Veteran" Yogyakarta</Text>
+              <ButtonComp/>
+            </div>
+            <div data-aos='fade-up' data-smooth-scroll-offset="180" data-aos-easing="ease" data-aos-duration="4000" data-aos-delay="1100">
+              <Heading color="white" textAlign="center" mt={50} fontSize={30} fontWeight="700" sx={{
+                '@media screen and (min-width: 823px)': {
+                  marginBottom: "100px"
+                },
+                '@media screen and (max-width: 823px)': {
+                  marginBottom: '20px'
+                }
+
+              }}>My Game</Heading>
+              <CardComp/>
+            </div>
         </Box>
 
       </div>
