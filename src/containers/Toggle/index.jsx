1f1824dcb97031f2df6a4360ab84027c3d0b35bd
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-07-12 06:27:29 +0800

    add styled toggle

---
+++ src/containers/Toggle/index.jsx
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+const Toggle = styled.button`
+    width: 50px;
+    height: 50px;
+    border-top-left-radius: 20%;
+    border-bottom-left-radius: 20%;
+    background: #fff;
+    border: 2px solid var(--bg-color);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    z-index: 1;
+    transition: all 0.3s ease-out;
+    right: 0%;
+    top: 40%;
+    box-shadow: 0 15px 24px rgba(0,0,0,0.11);
+    outline: none;
+    &:hover{
+        cursor: pointer;
+    }
+    svg {
+        color: black;
+    }
+`;
+
+export default Toggle;
