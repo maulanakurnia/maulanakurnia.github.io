2fc3e106dbec65bff3c4c680191ef0cb14bd2ed5
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-07-12 03:05:07 +0800

    remove public-ip

--- src/index.js
+++ src/index.js
@@ -3,15 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './components/pages/App/';
 import * as serviceWorker from './serviceWorker';
-const publicIp = require('public-ip');
-
 
 // Styling Console
 const warningTitleCSS = 'color:red; font-size:60px; font-weight: bold; -webkit-text-stroke: 1px black;';
 const warningDescCSS = 'font-size: 18px;';
 console.log('%cStop!', warningTitleCSS);
 console.log("%cThis is a browser feature intended for developers!!!", warningDescCSS);
-(async () => { console.log("Opps, Kami berhasil mengkap Public IP Adress anda {{ ", await publicIp.v4(), " }}"); })();
 
 
 ReactDOM.render(
