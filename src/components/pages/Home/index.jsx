5176faa10fc23ac40b9602a36a9e8fc7457541af
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-07-18 17:50:26 +0800

    change img avatar to div

--- src/components/pages/Home/index.jsx
+++ src/components/pages/Home/index.jsx
@@ -4,7 +4,6 @@ import {
     Text,
     Flex,
     Heading,
-    Avatar,
   } from "@primer/components";
 import Card from './HomeComp/Card';
 import uImage from "../../../assets/img/compress/bg-min.jpg";
@@ -12,11 +11,21 @@ import uAvatar from "../../../assets/img/compress/profile-min.jpeg"
 import ButtonSocial from "../../../containers/ButtonSocial"
 import Cover from '../../../containers/Cover/';
 import RepoGithub from './HomeComp/RepoGithub';
+import styled from 'styled-components';
 
 // Aos
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Footer from '../../Footer';
+
+const Profil = styled.div`
+    border-radius: 50%;
+    height: 200px;
+    width: 200px;
+    background-image: url(${props => props.imageUrl});
+    background-size: 200px 200px;
+    border: 1px solid #839296;
+`;
 
 class Home extends Component {
     componentDidMount(){	
@@ -31,7 +40,8 @@ class Home extends Component {
                 <Cover imageUrl={uImage}/>
                 <Box>
                     <Flex flexWrap="nowrap" justifyContent="center" sx={{ marginTop: '-100px' }}>
-                        <Avatar size={200} src={uAvatar} sx={{  borderRadius: '50%', borderBottom: "2px solid #839296", borderLeft: "1px solid #839296", borderRight: "1px solid #839296"}}/>
+                        <Profil imageUrl={uAvatar}/>
+                        {/* <Avatar size={200} src={uAvatar} sx={{  borderRadius: '50%', borderBottom: "2px solid #839296", borderLeft: "1px solid #839296", borderRight: "1px solid #839296"}}/> */}
                     </Flex>
                     <div data-aos='fade-up' data-smooth-scroll-offset="180" data-aos-easing="ease" data-aos-duration="4000" data-aos-delay="0">
                         <Heading as={Flex} justifyContent="center" mt={1} fontSize={30} fontWeight="700" sx={{
