1a8a14c4478386ae781f7b128302ce39127a0ab3
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-07-27 19:15:03 +0800

    make avatar to component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -4,7 +4,7 @@ import Card from './Card';
 import ButtonSocial from "../../components/ButtonSocial"
 import Cover from '../../components/Cover';
 import RepoGithub from './RepoGithub';
-import styled from 'styled-components';
+import Avatar from '../../components/Avatar';
 
 // Link Image
 import cover from "../../images/bg-min.jpg";
@@ -13,15 +13,6 @@ import avatar from "../../images/profile-min.jpeg"
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import Footer from '../../components/Footer';
-
-const Profil = styled.div`
-    border-radius: 50%;
-    height: 200px;
-    width: 200px;
-    background-image: url(${props => props.imageUrl});
-    background-size: 200px 200px;
-    border: 1px solid #839296;
-`;
 
 class Home extends Component {
     componentDidMount(){	
@@ -36,7 +27,7 @@ class Home extends Component {
                 <Cover imageUrl={cover}/>
                 <Box>
                     <Flex flexWrap="nowrap" justifyContent="center" sx={{ marginTop: '-100px' }}>
-                        <Profil imageUrl={avatar}/>
+                        <Avatar imageUrl={avatar}/>
                     </Flex>
                     <div data-aos='fade-up' data-smooth-scroll-offset="180" data-aos-easing="ease" data-aos-duration="4000" data-aos-delay="0">
                         <Heading as={Flex} justifyContent="center" mt={1} fontSize={30} fontWeight="700" sx={{
