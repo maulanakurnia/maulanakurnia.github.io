375dcd53923ae3c785434998b3e83d05c3422ebd
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-07-31 17:32:09 +0800

    remove styled container

--- src/pages/Home/RepoGithub.jsx
+++ src/pages/Home/RepoGithub.jsx
@@ -1,30 +1,9 @@
 import React, { Component } from "react";
-import styled from "styled-components";
 import axios from "axios";
 import { Box, Heading, Text, Flex } from "@primer/components";
 import Card from '../../components/Card'
-
-const Container = styled.div`
-  justify-content: center;
-  align-items: center;
-  padding: 0px 300px 0px 450px;
-
-  @media (min-width: 320px) and (max-width: 568px) {
-    padding: 0px 300px 0px 80px;
-  }
-  @media (min-width: 411px) and (max-width: 568px) {
-    padding: 0px 300px 0px 80px;
-  }
-  @media (max-width: 727px) {
-    padding: 0px 300px 0px 60px;
-  }
-  @media (max-width: 768px) {
-    padding: 0px 0px 0px 50px;
-  }
-  @media (min-width: 1024px) and (max-width: 1366px) {
-    padding: 0px 0px 0px 10px;
-  }
-`;
+import Wrapper from '../../components/Wrapper';
+import Container from '../../components/Container';
 
 class RepoGithub extends Component { 
   constructor(){
@@ -72,14 +51,7 @@ class RepoGithub extends Component {
           textAlign="center"
           fontSize={30}
           fontWeight="700"
-          sx={{
-            "@media screen and (min-width: 823px)": {
-              marginTop: "200px",
-            },
-            "@media screen and (max-width: 823px)": {
-              marginTop: "450px",
-            },
-          }}>
+          >
           My Repository
         </Heading>
         <Text
@@ -91,18 +63,20 @@ class RepoGithub extends Component {
           Total : {this.state.total}
         </Text>
         <Container>
-          {this.state.repos.map((repos)  => {
-            return (
-              <Card
-                key={repos.id}
-                title={repos.name}
-                description={repos.description}
-                language={repos.language}
-                langColors={this.languageColor(repos.language)}
-                onClick={() => window.open(repos.html_url, "_blank")}
-              />
-            );
-          })}
+          <Wrapper size={330}>
+            {this.state.repos.map((repos)  => {
+              return (
+                <Card
+                  key={repos.id}
+                  title={repos.name}
+                  description={repos.description}
+                  language={repos.language}
+                  langColors={this.languageColor(repos.language)}
+                  onClick={() => window.open(repos.html_url, "_blank")}
+                />
+              );
+            })}
+          </Wrapper>
         </Container>
       </Box>
     );
