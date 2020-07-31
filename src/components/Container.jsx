8466fec4f084808c240b60fb3855eefba714aafd
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-07-31 17:32:29 +0800

    add container

---
+++ src/components/Container.jsx
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export default styled.div`
+    width: 1200px;
+    margin: 0 auto;
+    @media only screen and (max-width: 1200px) {
+        // font-size: 12px;
+        width: 95%;
+    }
+`;
