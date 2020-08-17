98e80d1d4ba9ee2e27fd9e2a2fb75da6c35a72cf
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-08-17 15:03:14 +0800

    remove

--- src/__components/Wrapper.jsx
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export default styled.div`
-    display: grid;
-    grid-template-columns: repeat(auto-fit, ${props => props.size}px);
-    gap:5px;
-    justify-content: center;
-    padding-bottom: 5px;
-`;
