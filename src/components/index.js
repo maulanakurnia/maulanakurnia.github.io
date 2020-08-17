e07fc9d1a56955f302306b2a4a922ac6c6a89c7d
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-08-17 15:04:49 +0800

    add index

---
+++ src/components/index.js
@@ -0,0 +1,34 @@
+import {theme, darkTheme, lightTheme, BaseStyles} from './config'
+import Cover from './Cover'
+import Container from './Container'
+import Avatar from './Avatar'
+import Button, {Toggler, Toggle} from './Button'
+import Heading from './Heading'
+import Text from './Text'
+import {CardGame, RepoGithub} from './Card'
+import {Grid, Flex, Box} from './layout'
+import Footer from './Footer'
+export {
+    // Base
+    theme,
+    darkTheme,
+    lightTheme,
+    BaseStyles,
+
+    // Components
+    Cover,
+    Container,
+    Avatar,
+    Button,
+    Toggler,
+    Toggle,
+    Heading,
+    Text,
+    CardGame,
+    RepoGithub,
+    Footer,
+    Grid,
+    Flex,
+    Box
+
+}
