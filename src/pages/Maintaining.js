31b75044c65096fcf99cbcf6d5dd6e3cd10c1783
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-08-19 17:09:59 +0800

    [ CHANGE ] responsive & add button

--- src/pages/Maintaining.js
+++ src/pages/Maintaining.js
@@ -1,26 +1,15 @@
 import React from 'react';
 import styled from 'styled-components'
 import {AiFillSetting} from 'react-icons/ai'
+import {Container, Text, Flex, Button} from '../components'
+import { FaGithub, FaTwitter, FaInstagram } from "react-icons/fa";
 
-var Width = window.innerWidth;
-
-const Wrapper = styled.div`
-    position: absolute;
-    top: 40%;
-    max-width: 100%;
-    @media(max-width: ${Width}){
-        width: ${Width}
-    }
-    ${console.log(Width)}
-    // left: 40%;
-    width: 768px;
+const Wrapper = styled(Container)`
+    margin-top: 40vh;
     @media(max-width: 1020px){
         & > span {
             font-size: 17px!important;
         }
-    }
-    @media(min-width: 1020px){
-        left: 30%;
     }
     display: block;
     text-align: center;
@@ -33,7 +22,6 @@ const Wrapper = styled.div`
         font-size: 20px;
     }
     & > svg {
-        justify-content: center;
         font-size: 100px;
         animation: svg infinite 10s linear;
 
@@ -51,8 +39,14 @@ const Wrapper = styled.div`
 const Maintaining = () => {
     return(
     <Wrapper>
-        <AiFillSetting/>
-        <span><br></br>We'r working hard to bring New Version</span>
+            <AiFillSetting/>
+            <Text as="h3" sx={{marginBottom: '-15px'}}>Sorry</Text>
+            <span><br></br>We'r working hard to bring New Version</span>
+            <Flex justifyContent="center" mt={12}>
+                <Button primary onClick={()=> window.open("https://twitter.com/maulanaakurniaa", "_blank")}><FaTwitter/>Twitter</Button>            
+                <Button onClick={() => window.open("https://instagram.com/maulanaakurniaa", "_blank")}><FaInstagram/>Instagram</Button>            
+                <Button dark onClick={()=> window.open("https://github.com/maulanakurnia", "_blank")}><FaGithub/>Github</Button>            
+            </Flex>
     </Wrapper>)
 }
 
