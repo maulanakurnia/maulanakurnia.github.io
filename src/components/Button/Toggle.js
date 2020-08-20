325ab413355803ad4db3faecbad55f2e31fd634a
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-08-20 14:15:24 +0800

    add position toggle absolute

--- src/components/Button/Toggle.js
+++ src/components/Button/Toggle.js
@@ -1,6 +1,7 @@
+import React from 'react';
 import styled from 'styled-components'
 
-const Toggle = styled.div`
+const ToggleStyle = styled.div`
     width: 50px;
     height: 50px;
     border-radius: 20%;
@@ -23,5 +24,56 @@ const Toggle = styled.div`
         fill: ${({theme}) => theme.text};
     }
 `;
+
+const Toggle = ({children, ...rest}) => {
+    React.useEffect(()=> {
+        dragElement(document.getElementById("drag"));
+        function dragElement(elmnt) {
+        var posElmntX = 0, posElmntY = 0, posCursorX = 0, posCursorY = 0;
+        if (document.getElementById(elmnt.id )) {
+            /* if present, the header is where you move the DIV from:*/
+            document.getElementById(elmnt.id).onmousedown = dragMouseDown;
+        } else {
+            /* otherwise, move the DIV from anywhere inside the DIV:*/
+            elmnt.onmousedown = dragMouseDown;
+        }
+
+        function dragMouseDown(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // get the mouse cursor position at startup:
+            posCursorX = e.clientX;
+            posCursorY = e.clientY;
+            document.onmouseup = closeDragElement;
+            // call a function whenever the cursor moves:
+            document.onmousemove = elementDrag;
+        }
+
+        function elementDrag(e) {
+            e = e || window.event;
+            e.preventDefault();
+            // calculate the new cursor position:
+            posElmntX = posCursorX - e.clientX;
+            posElmntY = posCursorY - e.clientY;
+            posCursorX = e.clientX;
+            posCursorY = e.clientY;
+
+            // set the element's new position:
+            elmnt.style.left = (elmnt.offsetLeft - posElmntX) + "px";
+            elmnt.style.top = (elmnt.offsetTop - posElmntY) + "px";
+        }
+
+        function closeDragElement() {
+            document.onmouseup = null;
+            document.onmousemove = null;
+        }
+        }
+    })
+    return(
+        <ToggleStyle {...rest} id="drag">
+            {children}
+        </ToggleStyle>
+    );
+}
 
 export default Toggle;
