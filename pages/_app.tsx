2503b9261f9feeba89ddd8417c63060531fcaffb
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-08-27 21:34:25 +0800

    change to typeScript

---
+++ pages/_app.tsx
@@ -0,0 +1,13 @@
+import type { AppProps } from 'next/app'
+import { ThemeProvider, CSSReset } from '@chakra-ui/core'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider>
+      <CSSReset />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  )
+}
+
+export default MyApp
--- src/components/Container.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components';
-
-export default styled.div`
-    width: 1200px;
-    margin: 0 auto;
-    @media only screen and (max-width: 1200px) {
-        width: 95%;
-    }
-`;
