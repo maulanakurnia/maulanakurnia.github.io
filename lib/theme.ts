afec83e7f19557ddcb663dcf7ad7c2271bc6c54a
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-08-28 23:48:48 +0800

    add themes

---
+++ lib/theme.ts
@@ -0,0 +1,27 @@
+// theme.js
+import theme from "@chakra-ui/theme"
+import { merge } from "@chakra-ui/utils"
+
+// You'll need the @chakra-ui/theme-tools to have access to the mode function
+import { mode } from "@chakra-ui/theme-tools"
+
+// Remember, you can access special theme variables if you
+// declare a style as a function, rather than an object.
+const overrides = {
+  styles: {
+    global: (props) => ({
+      body: {
+        fontFamily: "body",
+        color: mode("gray.800", "whiteAlpha.900")(props),
+        bg: mode("white", "#121212")(props),
+        lineHeight: "base",
+      },
+      
+    }),
+  },
+}
+
+// WARNING: If you do use a function in a style override, you will NOT
+// inherit the existing styles like you will with an object.
+// merge() will only merge objects, not functions
+export default merge(theme, overrides)
