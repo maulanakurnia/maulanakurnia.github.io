016b4cb16a4bcdf916f4da75b48206ef6da844f9
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-08-28 19:37:41 +0800

    clean up

--- src/components/Button/index.js
+++
@@ -1,4 +0,0 @@
-import Button from './Button'
-export default Button
-export {Toggler} from './ButtonToggler'
-export {default as Toggle} from './Toggle'
--- src/components/Card/index.js
+++
@@ -1,2 +0,0 @@
-export {default as CardGame} from './CardGame'
-export {default as RepoGithub} from './RepoGithub'
