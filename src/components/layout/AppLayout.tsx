c619f70f3c06c24a83704dab515b9cbb3453479a
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-08-28 23:51:09 +0800

    App layout

---
+++ src/components/layout/AppLayout.tsx
@@ -0,0 +1,18 @@
+import { ChakraProvider, CSSReset, Box } from '@chakra-ui/core'
+import theme from '@chakra-ui/theme'
+import customeTheme from '../../../lib/theme'
+import Header from './Header'
+
+function AppLayout({ children, }) {
+    return (
+        <ChakraProvider theme={customeTheme}>
+            <CSSReset />
+            <Header/>
+            <Box as='main' className='main'>
+            {children}
+            </Box>
+        </ChakraProvider>
+    )
+}
+
+export default AppLayout
