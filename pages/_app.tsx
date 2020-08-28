e174bf7e48fc3eb658a960849468f6ca4baac0d8
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-08-28 23:49:05 +0800

    layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,27 @@
+import * as gtag from '../lib/gtag'
+import { Router } from 'next/router'
 import type { AppProps } from "next/app";
-import {useEffect, useState} from 'react'
-import { ChakraProvider, CSSReset } from "@chakra-ui/core";
-import theme from "@chakra-ui/theme";
-
+import { useEffect, useState } from 'react'
+import AppLayout from 'components/layout/AppLayout'
 
 function App({Component, pageProps }:AppProps) {
     const [mounted, setMounted] = useState(false);
 
     useEffect(() => {
       setMounted(true);
+      const handleRouteChange = (url) => {
+        gtag.pageview(url)
+      }
+      Router.events.on('routeChangeComplete', handleRouteChange)
+      return () => {
+        Router.events.off('routeChangeComplete', handleRouteChange)
+      }
     }, []);
 
   return (
-    <ChakraProvider theme={theme}>
-        <CSSReset />
-        {mounted && <Component {...pageProps} />}
-    </ChakraProvider>
+        <AppLayout>
+            {mounted && <Component {...pageProps} />}
+        </AppLayout>
   );
 }
 
