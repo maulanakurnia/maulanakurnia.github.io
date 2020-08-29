66ea479c325c53cf1c4f90ec119215a0f8c6c0e6
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-08-29 15:36:24 +0800

    add page header

---
+++ src/components/pageHeader.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Heading } from '@chakra-ui/core'
+
+interface Props {
+  title: string;
+  simple: boolean;
+  hasB: boolean;
+}
+function PageHeader({ title, simple, hasB }: Partial<Props>) {
+
+  if (simple) {
+    return <Heading as='h1' mb={hasB && 5}>{title}</Heading>
+  }
+  return (
+    <Heading as='h1' px={[15,20]} fontSize={[25, 30]} mb={[5, 10]} pt={"70px"}>{title}</Heading>
+  )
+}
+
+export default PageHeader
