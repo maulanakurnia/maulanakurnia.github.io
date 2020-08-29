9984315d01b90974d33ba04ab097417417157afe
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-08-29 15:36:08 +0800

    add blog

--- pages/blog/index.tsx
+++ pages/blog/index.tsx
@@ -1,87 +1,84 @@
-
-// import { Box, Flex, Heading, Text, useColorModeValue } from "@chakra-ui/core";
-// import fs from "fs";
-// import matter from 'gray-matter';
-// import Link from "next/link";
-// import path from "path";
-// import timeRead from 'read-time';
-// import { Fragment } from "react";
-// import AuthorCard from 'components/blog/AuthorCard';
-// import SEO from 'components/seo';
-// import PageHeader from 'components/page-header';
-// import { dateFormat } from 'lib/dateFormat';
-
+import {
+  Box,
+  Text,
+  useColorModeValue,
+  Stack,
+  Badge
+} from "@chakra-ui/core";
 import { Fragment } from "react";
 import SEO from "components/utils/seo";
+import PageHeader from "components/pageHeader";
+import NextLink from 'next/link';
 
 function Blog({ posts }) {
-    return (
-        <Fragment>
-            <SEO title = "Blog"/>
-        </Fragment>
-    );
+  const bg = useColorModeValue("#fff", "#000");
+  return (
+    <Fragment>
+      <SEO title="Blog" />
+      <PageHeader title="Blog" />
+      <Box px={[5,20]} display="flex" flexDirection={{xs: 'column', lg: 'row'}} justifyContent="space-between">
+        <Box bg={bg} borderRadius={6} py={10}>
+          <Box px={[0,5]} py={2}>
+            <Box display="flex" _hover={{bg: 'hsla(0,0%,50%,.125)'}} px={{xs:5, lg:10}} p={{lg: 10}} borderRadius={6} justifyContent="space-between">
+                <Box w="full"display={{xs: 'none', lg: 'block'}}>
+                    <Text as="span" fontSize={{xs: 13, lg: 16}} my="auto">
+                        12 Juni 2020
+                    </Text>
+                </Box>
+                <Box w="500%">
+                    <Text fontWeight="600" fontSize={{xs: 16, lg:20}}>
+                        <NextLink href="/">Title here</NextLink>
+                    </Text>
+                    <Text as="span" fontSize={12} my="auto" display={{xs: 'block', lg: 'none'}}>
+                        12 Juni 2020
+                    </Text>
+                    <Stack direction="row" py={2}>
+                        <Badge>Tag</Badge>
+                        <Badge colorScheme="green">Tag</Badge>
+                        <Badge colorScheme="red">Tag</Badge>
+                        <Badge colorScheme="purple">Tag</Badge>
+                    </Stack>
+                    <Text className="description" fontSize={{xs: 13, lg:18}} textAlign="justify">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eius
+                        quia beatae, non vero rem magnam pariatur accusamus nobis
+                        consequatur doloribus dolor perferendis impedit libero eveniet
+                        fugit, totam aliquid iure quas ratione inventore a!
+                        Voluptatibus sunt hic et cumque ipsa maiores necessitatibus
+                        optio, ipsum esse possimus porro placeat minus. Repudiandae,
+                        dolores laboriosam. Harum vel eveniet facilis commodi
+                        cupiditate totam, accusamus assumenda aliquid quae non libero
+                        dolorum blanditiis molestias eum neque explicabo aut nesciunt?
+                        Tempore, tempora eveniet, veritatis reprehenderit enim soluta
+                        incidunt vel, hic excepturi quae dolorem nam corporis
+                        blanditiis. Aperiam assumenda, dicta neque ducimus ipsum
+                        suscipit distinctio. Quisquam modi perferendis eligendi.
+                    </Text>
+                </Box>
+            </Box>
+
+          </Box>
+        </Box>
+        <Box as="aside" pl={{xs:0, lg:20}} position="sticky" top="5%" width='full' overflow="auto">
+            <Box bg={bg} borderRadius={6} p={5} mt={{xs: 5, lg: 0}}>
+                <Text fontWeight="600">Tags</Text>
+                <Stack direction="row" py={2}>
+                    <Badge>Tag</Badge>
+                    <Badge colorScheme="green">Tag</Badge>
+                    <Badge colorScheme="red">Tag</Badge>
+                    <Badge colorScheme="purple">Tag</Badge>
+                    <Badge colorScheme="green">Tag</Badge>
+                    <Badge colorScheme="red">Tag</Badge>
+
+                </Stack>
+            </Box>
+            <Box bg={bg} borderRadius={6} p={5} mt={{xs: 5, lg: 4}}>
+                <Text fontWeight="600">Artikel Terbaru</Text>
+
+            </Box>
+        </Box>
+      </Box>
+    </Fragment>
+  );
 }
 
 export default Blog;
-
-
-// function Blog({ posts }) {
-//   const description = useColorModeValue('gray.600', 'gray.400')
-//   const titleC = useColorModeValue('black', 'gray.300')
-
-
-//   return (
-//     <Fragment>
-//       <SEO title='Blog Posts' />
-
-//       <Box p={3}>
-//         <PageHeader title='Thoughts' />
-//         {posts.map((post) => {
-//           const { title, author } = post.data
-
-//           return (
-//             <Box key={title} width={['', '', 800]} mx='auto' cursor='pointer'>
-//               <Link href={post.slug.replace('.mdx', '')}>
-
-//                 <Flex justify='space-around' direction='column' p={6} >
-//                   <Heading fontWeight={500} mb={4} fontSize={30} color={titleC} >{title}</Heading>
-
-//                   <Text color={description} fontSize={17} mb={4} >{post?.data.description}</Text>
-
-//                   <Flex justify='space-between' alignItems='center' color='gray.500' fontSize={14}>
-//                     <AuthorCard author={author} withAvatar={false} />
-//                     <Text rounded='lg' >{post?.timeToRead.m + 1}  min read</Text>
-//                   </Flex>
-//                 </Flex>
-
-//               </Link>
-//             </Box>
-//           )
-//         })}
-//       </Box>
-
-//     </Fragment >
-//   )
-// }
-
-
-
-
-// export const getStaticProps = async () => {
-//   const files = fs.readdirSync("posts").filter(name => !name.includes('.DS_Store'))
-//   const postMatter = file => matter(fs.readFileSync(path.join("posts", file), 'utf-8'))
-
-//   const posts = files.map(filename => ({
-//     timeToRead: timeRead(postMatter(filename).content),
-//     data: postMatter(filename).data,
-//     slug: 'blog/' + filename
-//   }))
-
-//   return {
-//     props: {
-//       posts: posts.sort((a, b) => dateFormat(a.data.date).unix() + dateFormat(b.data.date).unix())
-//     }
-//   }
-// };
-
-// export default Blog;
