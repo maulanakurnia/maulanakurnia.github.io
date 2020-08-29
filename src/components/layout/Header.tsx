95ef33ee672aaf7fefbe802096e0f86f64a13308
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-08-29 17:41:51 +0800

    change layout

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,8 +1,9 @@
-import { Box, Link, Button, useColorMode, useColorModeValue, IconButton } from "@chakra-ui/core";
+import { Box, Link, Button, useColorMode, InputGroup, Input, InputRightElement, IconButton, useColorModeValue } from "@chakra-ui/core";
 import { RiMenuLine, RiCloseLine } from "react-icons/ri";
 import { useState } from "react";
 import { FiMoon, FiSun, FiSearch } from "react-icons/fi";
 import ActiveLink from '../utils/Active-link';
+import { BsSlashSquare } from 'react-icons/bs';
 
 const MenuItems = ({ children, to }: any) => {
   return (
@@ -13,7 +14,12 @@ const MenuItems = ({ children, to }: any) => {
         fontWeight="500"
         fontSize={15} 
         display={["block", "flex"]} 
-        borderTopWidth={["1px", "0"]}>
+        my="auto"
+        borderTopWidth={["1px", "0"]}
+        transition="all 0.2s"
+        borderRadius={{xs: '0px', md:'6px'}}
+        className="css-xhsysdk"
+        _hover={{ bg: useColorModeValue("gray.100", "whiteAlpha.100") }}>
         {children}
       </Link>
     </ActiveLink>
@@ -23,20 +29,14 @@ const MenuItems = ({ children, to }: any) => {
 const Header = () => {
     const [open, setOpen] = useState(false);
     const { colorMode, toggleColorMode } = useColorMode();
-    const bgColor = useColorModeValue('white', '#121212');
-    const color = useColorModeValue('#363537', '#929497');
-    const boxShadow = useColorModeValue('#000', '#fff');
     return ( 
         <Box // Header
             as="header"
-            bg={bgColor}
-            color={color}
             w="100%"
             p={2}
             px={5}
             position="fixed"
-            zIndex={999}
-            boxShadow={"0px -20px 27px" + boxShadow}>
+            zIndex={999}>
             <Box // Header Content
                 display="flex"
                 flexDirection={["column","row"]}
@@ -58,7 +58,7 @@ const Header = () => {
                             onClick={toggleColorMode}
                             _focus={{outline: 0}}
                             fontSize={14}>
-                            {colorMode === "light" ? <FiSun color="#000"/> : <FiMoon />}
+                            {colorMode === "light" ? <FiSun/> : <FiMoon />}
                         </Button>
                         <Button
                             size="xs"
@@ -85,8 +85,18 @@ const Header = () => {
                             py={[3,0]}
                             display="flex"
                             flexDirection={["column","row"]}
-                            justifyItems="center"
                             w="full">
+                            <InputGroup w={"15%"} size="sm" mx={5} isDisabled={true} display={{xs: 'none', md:'block'}} variant="filled" className="inputGroup">
+                                <InputRightElement className="custom_Icon" children={<BsSlashSquare size={100} color="gray.300" />} fontSize="120px"/>
+                                <Input 
+                                    type="text" 
+                                    placeholder="Coming Soon" 
+                                    borderRadius={6} 
+                                    h="24px"
+                                    fontSize={12} 
+                                    className="custom_Search"
+                                    _focus={{outline: 'none'}}/>
+                            </InputGroup>
                             <MenuItems to='/'>Home</MenuItems>
                             <MenuItems to='/blog'>Blog</MenuItems>
                             <MenuItems to='/project'>Project</MenuItems>
