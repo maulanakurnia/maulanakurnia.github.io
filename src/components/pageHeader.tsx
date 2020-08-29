203fa492a6796ee216e39f267b96517aef773693
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-08-29 23:36:10 +0800

    remove has border

--- src/components/pageHeader.tsx
+++ src/components/pageHeader.tsx
@@ -4,12 +4,11 @@ import { Heading } from '@chakra-ui/core'
 interface Props {
   title: string;
   simple: boolean;
-  hasB: boolean;
 }
-function PageHeader({ title, simple, hasB }: Partial<Props>) {
+function PageHeader({ title, simple }: Partial<Props>) {
 
   if (simple) {
-    return <Heading as='h1' mb={hasB && 5}>{title}</Heading>
+    return <Heading as='h1' mb={5}>{title}</Heading>
   }
   return (
     <Heading as='h1' px={[15,20]} fontSize={[25, 30]} mb={[5, 10]} pt={"70px"}>{title}</Heading>
