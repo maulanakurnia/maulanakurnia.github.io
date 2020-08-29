9fe747e4d95dcce4c42906cc50cacc42123a960f
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-08-29 23:34:43 +0800

    theming

--- lib/theme.ts
+++ lib/theme.ts
@@ -1,32 +1,33 @@
-// theme.js
-import theme from "@chakra-ui/theme"
-import { merge } from "@chakra-ui/utils"
+import { mode, Styles } from "@chakra-ui/theme-tools";
+import theme from "@chakra-ui/theme";
 
-// You'll need the @chakra-ui/theme-tools to have access to the mode function
-import { mode } from "@chakra-ui/theme-tools"
-
-// Remember, you can access special theme variables if you
-// declare a style as a function, rather than an object.
-
-const overrides = {
-  styles: {
-    global: (props) => ({
-      body: {
+const styles: Styles = {
+  ...theme.styles,
+  global: (props) => ({
+    ...theme.styles.global,
+    color: mode("blackAlpha.900", "whiteAlpha.900")(props),
+    bg: mode("#ffffff", "#031e3e")(props),
+    body: {
         fontFamily: "body",
         color: mode("#363537", "#929497")(props),
         bg: mode("#FFF", "#121212")(props),
         lineHeight: "base",
-      },
-      header: {
+    },
+    header: {
           bg: mode("#FFF", "#121212")(props),
           color: mode('#363537', '#929497')(props),
           boxShadow: mode("0px -20px 20px #000", "0px -20px 20px #FFF")(props)
-      }
-    }),
-  },
-}
+    }
+  }),
+};
 
-// WARNING: If you do use a function in a style override, you will NOT
-// inherit the existing styles like you will with an object.
-// merge() will only merge objects, not functions
-export default merge(theme, overrides)
+const customTheme: any = {
+  ...theme,
+  config: {
+    ...theme.config,
+    useSystemColorMode: true,
+    initialColorMode: "dark",
+  },
+  styles,
+};
+export default customTheme;
