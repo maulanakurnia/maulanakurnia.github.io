2bfe356f8ec64cf60d9e1b442b7e40b2d7ca7ccd
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-08-29 15:36:44 +0800

    fixing header

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,14 +1,19 @@
-import { Box, Link, Button, useColorMode, IconButton } from "@chakra-ui/core";
+import { Box, Link, Button, useColorMode, useColorModeValue, IconButton } from "@chakra-ui/core";
 import { RiMenuLine, RiCloseLine } from "react-icons/ri";
 import { useState } from "react";
-import NextLink from "next/link";
-import { FiMoon, FiSun } from "react-icons/fi";
+import { FiMoon, FiSun, FiSearch } from "react-icons/fi";
 import ActiveLink from '../utils/Active-link';
 
 const MenuItems = ({ children, to }: any) => {
   return (
     <ActiveLink href={to}>
-      <Link px={10} color="gray.500">
+      <Link 
+        userSelect="none"
+        px={[0,2]} py={[1,0]} 
+        fontWeight="500"
+        fontSize={15} 
+        display={["block", "flex"]} 
+        borderTopWidth={["1px", "0"]}>
         {children}
       </Link>
     </ActiveLink>
@@ -16,79 +21,90 @@ const MenuItems = ({ children, to }: any) => {
 };
 
 const Header = () => {
-  const [open, setOpen] = useState(false);
-  const { colorMode, toggleColorMode } = useColorMode();
-  const bgColor = { light: "white", dark: "#121212" };
-  const color = { light: "#363537", dark: "#929497" };
+    const [open, setOpen] = useState(false);
+    const { colorMode, toggleColorMode } = useColorMode();
+    const bgColor = useColorModeValue('white', '#121212');
+    const color = useColorModeValue('#363537', '#929497');
+    const boxShadow = useColorModeValue('#000', '#fff');
+    return ( 
+        <Box // Header
+            as="header"
+            bg={bgColor}
+            color={color}
+            w="100%"
+            p={2}
+            px={5}
+            position="fixed"
+            zIndex={999}
+            boxShadow={"0px -20px 27px" + boxShadow}>
+            <Box // Header Content
+                display="flex"
+                flexDirection={["column","row"]}
+                justifyContent="space-between">
+                <Box // Brand & Button
+                    display="flex" 
+                    justifyContent="space-between"
+                    w={["full","auto"]}>
+                    <Box>
+                        {/* Brand */}
+                        <Link fontWeight="600" href="/" fontSize={15}>mufradmabni</Link>
+                    </Box>
+                    <Box
+                        display={["flex","none"]}>
+                        {/* Humberger */}
+                        <Button
+                            size="xs"
+                            variant="ghost"
+                            onClick={toggleColorMode}
+                            _focus={{outline: 0}}
+                            fontSize={14}>
+                            {colorMode === "light" ? <FiSun color="#000"/> : <FiMoon />}
+                        </Button>
+                        <Button
+                            size="xs"
+                            variant="ghost"
+                            _focus={{outline: 0}}
+                            fontSize={14}>
+                            {<FiSearch/>}
+                        </Button>
+                        <Button
+                            size="xs"
+                            variant="ghost"
+                            onClick={() => setOpen(!open)}
+                            _focus={{outline: 0}}
+                            fontSize={14}>
+                            {open ? <RiCloseLine /> : <RiMenuLine />}
+                        </Button>
+                    </Box>
+                </Box>
+                    <Box // Content
+                        display={[open ? "flex" : "none", "flex"]}
+                        justifyContent="space-between"
+                        w="full"> 
+                        <Box 
+                            py={[3,0]}
+                            display="flex"
+                            flexDirection={["column","row"]}
+                            justifyItems="center"
+                            w="full">
+                            <MenuItems to='/'>Home</MenuItems>
+                            <MenuItems to='/blog'>Blog</MenuItems>
+                            <MenuItems to='/project'>Project</MenuItems>
+                        </Box>
 
-  return (
-    <Box
-      as="header"
-      bg={bgColor[colorMode]}
-      color={color[colorMode]}
-      w="100%"
-      p={10}
-      px={30}
-      position="fixed"
-      zIndex={999}
-      mb={30}
-      boxShadow="0px -20px 27px #000"
-      borderBottomWidth="1px"
-    >
-      <Box display="flex" w="100%" flexDirection={["column", "row"]}>
-        <Box
-          display="flex"
-          flexDirection={{ xs: "row" }}
-          justifyContent="space-between"
-          px={1}
-          w={{ sm: "100%", md: "" }}
-        >
-          <Box>
-            <NextLink href="/">mufradmabni</NextLink>
-          </Box>
-          <Box my="auto" display={["flex", "none"]}>
-            <IconButton
-              pr={10}
-              display={["flex", "none"]}
-              aria-label="toggle mode"
-              size="xs"
-              icon={colorMode === "light" ? <FiSun /> : <FiMoon />}
-              onClick={toggleColorMode}
-              variant="ghost"
-            />
-            <Button
-              size="xs"
-              variant="ghost"
-              onClick={() => setOpen(!open)}
-              cursor="pointer"
-            >
-              {open ? <RiCloseLine /> : <RiMenuLine />}
-            </Button>
-          </Box>
+                        <Box> {/* Toggle Theme */}
+                            <IconButton
+                                display={["none", "flex"]}
+                                aria-label="toggle mode"
+                                size="xs"
+                                icon={colorMode === "light" ? <FiSun /> : <FiMoon />}
+                                onClick={toggleColorMode}
+                                variant="ghost"/>
+                        </Box>
+                </Box>
+            </Box>
         </Box>
-        <Box
-          display={[open ? "flex" : "none", "flex"]}
-          justifyContent="space-between"
-          w="100%"
-        >
-          <Box px={1} display={["block", "flex"]}>
-            <MenuItems to='/'>Home</MenuItems>
-            <MenuItems to='/blog'>Blog</MenuItems>
-          </Box>
-          <Box display={{ sx: "none", md: "block" }} my={4}>
-            <IconButton
-              display={["none", "block"]}
-              aria-label="toggle mode"
-              size="xs"
-              icon={colorMode === "light" ? <FiSun /> : <FiMoon />}
-              onClick={toggleColorMode}
-              variant="ghost"
-            />
-          </Box>
-        </Box>
-      </Box>
-    </Box>
-  );
+    );
 }
 
 export default Header;
