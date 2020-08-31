b40a8e4c263f7d06f55b1106ea613079298691c1
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-09-01 00:21:33 +0800

    add nprogress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
 import type { AppProps } from "next/app";
 import { AppLayout } from 'templates/index';
 import Maintaining from "./maintaining";
+import { Router } from "next/router";
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css';
+
+Router.events.on('routeChangeStart', ()=>{
+    NProgress.start();
+})
+Router.events.on('routeChangeComplete', ()=>{
+    NProgress.done();
+})
+Router.events.on('routeChangeError', ()=>{
+    NProgress.done();
+})
 
 function App({Component, pageProps }:AppProps) {
     var MAINTAINING: boolean = false;
