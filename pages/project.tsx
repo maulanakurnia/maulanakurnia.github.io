04367f488dd4f19472d7b751032efd891ca92a73
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-09-01 00:23:13 +0800

    styling

--- pages/project.tsx
+++ pages/project.tsx
@@ -1,19 +1,19 @@
 import { Fragment } from "react";
 import SEO from "src/components/atoms/seo";
 import PageHeader from "src/components/atoms/pageHeader";
-import { Box,useColorModeValue, Text } from '@chakra-ui/core'
+import { Box,useColorModeValue, Text, Container } from '@chakra-ui/core'
 
 export default function Project() {
     const bg = useColorModeValue("1px solid #dadce0", "1px solid rgb(39, 41, 46)");
     return(
         <Fragment>
             <SEO title="Blog" />
-            <PageHeader title="Project" />
-            <Box px={[5, 20]}>
+            <Container maxW="xl">
+                <PageHeader title="Project" />
                 <Box border={bg} borderRadius={6} p={10} w="full">
                     <Text fontWeight="700">COMING SOON</Text>
                 </Box>
-            </Box>
+            </Container>
         </Fragment>
     )
 }
