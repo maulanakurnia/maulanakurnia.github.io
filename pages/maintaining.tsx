045fd0ee69aac90179e03441e6b28d73df49ef86
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-09-01 00:22:52 +0800

    maintaingin page

--- pages/maintaining.tsx
+++ pages/maintaining.tsx
@@ -10,7 +10,7 @@ export default function Maintaining(){
             <Helmet>
                 <link href="http://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet" type="text/css"/> 
             </Helmet>
-            <GlobalStyle/>
+
             <Center height="100vh">
                 <Box position="absolute" top="40%" textAlign="center" justifyContent="center" width="100%" css={css`
                     &>svg {
