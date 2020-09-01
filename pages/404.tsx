48e52ad3e8deeb67ee28771af4b7eddb7a48866e
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-09-02 01:35:48 +0800

    change position

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,13 +1,15 @@
 import React from "react"
-import {Text,Heading, Container} from '@chakra-ui/core'
+import {Text,Heading, Container, useColorModeValue} from '@chakra-ui/core'
 import { NextSeo } from "next-seo"
 const NotFoundPage = () => (
   <>
     <NextSeo
         title="404" 
-        description="page not found"/>
-    <Container textAlign="center">
-        <Heading fontSize={[25, 30]} mb={[2, 5]} pt={"70px"}>NOT FOUND</Heading>
+        description="page not found"
+        titleTemplate="%s | mufradmabni"
+        noindex={true}/>
+    <Container textAlign="center" top="30%" left="35%" position="absolute" color={useColorModeValue("#c40707","#802b2b")}>
+        <Heading fontSize={[25, 30]} mb={[2, 5]} pt={"70px"}>404 NOT FOUND</Heading>
         <Text>You just hit a route that doesn&#39;t exist... the sadness.</Text>
     </Container>
   </>
