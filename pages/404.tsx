b52294c0c8eadb85da005a7d1da01e447b75973c
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-09-03 23:36:44 +0800

    change to function

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,18 +1,27 @@
-import React from "react"
-import {Text,Heading, Container, useColorModeValue} from '@chakra-ui/core'
-import { NextSeo } from "next-seo"
-const NotFoundPage = () => (
-  <>
-    <NextSeo
-        title="404" 
-        description="page not found"
-        titleTemplate="%s | mufradmabni"
-        noindex={true}/>
-    <Container textAlign="center" top="30%" left="35%" position="absolute" color={useColorModeValue("#c40707","#802b2b")}>
-        <Heading fontSize={[25, 30]} mb={[2, 5]} pt={"70px"}>404 NOT FOUND</Heading>
-        <Text>You just hit a route that doesn&#39;t exist... the sadness.</Text>
-    </Container>
-  </>
-)
-
-export default NotFoundPage
+import React from "react";
+import { Text, Heading, Container, useColorModeValue } from "@chakra-ui/core";
+import { NextSeo } from "next-seo";
+export default function NotFoundPage() {
+    return(
+        <>
+            <NextSeo
+            description="page not found"
+            noindex={true}
+            title="404"
+            titleTemplate="%s | mufradmabni"
+            />
+            <Container
+            color={useColorModeValue("#c40707", "#802b2b")}
+            left="35%"
+            position="absolute"
+            textAlign="center"
+            top="30%"
+            >
+            <Heading fontSize={[25, 30]} mb={[2, 5]} pt={"70px"}>
+                404 NOT FOUND
+            </Heading>
+            <Text>You just hit a route that doesn&#39;t exist... the sadness.</Text>
+            </Container>
+        </>
+    )
+};
