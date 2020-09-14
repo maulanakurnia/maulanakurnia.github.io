5f48daded712680584ff284a7eb60c2ab1c21196
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-09-14 09:37:35 +0800

    next config

--- next.config.js
+++ next.config.js
@@ -1,34 +1,35 @@
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
+    enabled: process.env.ANALYZE === "true",
 });
-
 module.exports = withBundleAnalyzer({
-  env: {
-    HOMEPAGE: process.env.URL || process.env.VERCEL_URL,
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.node = {
-        fs: "empty",
-      };
-    }
+    env: {
+        HOMEPAGE: process.env.URL || process.env.VERCEL_URL,
+    },
+    webpack: (config, {
+        isServer
+    }) => {
+        if (!isServer) {
+            config.node = {
+                fs: "empty",
+            };
+        }
 
-    config.module.rules = [
-      ...config.module.rules,
-      {
-        test: /\.(ttf)$/i,
-        loader: "file-loader",
-      },
-      {
-        test: /\.(frag|vert|glsl)$/i,
-        loader: "glsl-shader-loader",
-      },
-      {
-        test: /\.mdx?$/,
-        use: ["babel-loader", "@mdx-js/loader"],
-      },
-    ];
+        config.module.rules = [
+            ...config.module.rules,
+            {
+                test: /\.(ttf)$/i,
+                loader: "file-loader",
+            },
+            {
+                test: /\.(frag|vert|glsl)$/i,
+                loader: "glsl-shader-loader",
+            },
+            {
+                test: /\.mdx?$/,
+                use: ["babel-loader", "@mdx-js/loader"],
+            },
+        ];
 
-    return config;
-  },
+        return config;
+    },
 });
