e94ecff4b795c697c55f1076b0b744b15124db47
Maulana Kurnia <maulanaakurniaa@yahoo.com>
2020-09-17 15:22:14 +0800

    icon link

---
+++ src/components/molecules/IconLink.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { IconButton, Link } from "@chakra-ui/core";
+import { TypeIconLink } from "types/TypeIconLink";
+
+export const IconLink: React.FC<TypeIconLink> = ({ icon, href }) => (
+  <Link display="inline-block" mx="5px" href={href} isExternal>
+    <IconButton
+      aria-label="social icon"
+      icon={icon}
+      borderRadius="50%"
+      size="md"
+      color="gray.500"
+      variant="ghost"
+    />
+  </Link>
+);
